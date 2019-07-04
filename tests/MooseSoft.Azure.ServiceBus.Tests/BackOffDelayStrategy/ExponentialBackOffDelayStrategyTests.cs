43418406a73c028bd9cac484c0f1b5eb51c7cf6e
Marty Mathis <Marty.Mathis@assurant.com>
2019-07-04 11:11:52 -0400

    Unit tests for back off delay strategies

---
+++ tests/MooseSoft.Azure.ServiceBus.Tests/BackOffDelayStrategy/ExponentialBackOffDelayStrategyTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MooseSoft.Azure.ServiceBus.BackOffDelayStrategy;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MooseSoft.Azure.ServiceBus.Tests.BackOffDelayStrategy
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class ExponentialBackOffDelayStrategyTests
+    {
+        [TestMethod]
+        public void Calculate_Test()
+        {
+            //Arrange
+            var sut = ExponentialBackOffDelayStrategy.Default;
+            var results = new List<TimeSpan>();
+
+            //Act
+            for (var i = 1; i <= 10; i++)
+            {
+                results.Add(sut.Calculate(i));
+            }
+            
+            //Assert
+            var hour = TimeSpan.FromHours(1);
+            for (var i = 0; i < 10; i++)
+            {
+                if (i < 5)
+                    Assert.IsTrue(TimeSpan.Zero < results[i] && hour > results[i]);
+                else
+                    Assert.IsTrue(hour == results[i]);
+            }
+        }
+    }
+}
---
+++ tests/MooseSoft.Azure.ServiceBus.Tests/BackOffDelayStrategy/LinearBackOffDelayStrategyTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MooseSoft.Azure.ServiceBus.BackOffDelayStrategy;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace MooseSoft.Azure.ServiceBus.Tests.BackOffDelayStrategy
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class LinearBackOffDelayStrategyTests
+    {
+        [TestMethod]
+        public void Calculate_Test()
+        {
+            //Arrange
+            var sut = LinearBackOffDelayStrategy.Default;
+            var results = new List<TimeSpan>();
+
+            //Act
+            for (var i = 1; i <= 10; i++)
+            {
+                results.Add(sut.Calculate(i));
+            }
+            
+            //Assert
+            // ReSharper disable once CompareOfFloatsByEqualityOperator
+            Assert.IsTrue(results.All(ts => (ts.TotalMinutes % 2) == 0 ));
+
+            var previous = TimeSpan.Zero;
+            foreach (var ts in results)
+            {
+                Assert.AreEqual(previous + TimeSpan.FromMinutes(2), ts);
+                previous = ts;
+            }
+        }
+    }
+}
