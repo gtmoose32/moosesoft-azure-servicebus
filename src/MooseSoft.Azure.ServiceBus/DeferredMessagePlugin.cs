6ab5a21f2d900747eff6bb0469747cb7e684230a
Marty Mathis <Marty.Mathis@assurant.com>
2020-03-10 07:16:08 -0400

    Added DeferredMessagePlugin to handle deferred message failure policy through use of ServiceBusPlugin.

---
+++ src/MooseSoft.Azure.ServiceBus/DeferredMessagePlugin.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using System.Threading.Tasks;
+
+namespace MooseSoft.Azure.ServiceBus
+{
+    public class DeferredMessagePlugin : ServiceBusPlugin
+    {
+        private readonly IMessageReceiver _messageReceiver;
+        public override string Name => nameof(DeferredMessagePlugin);
+
+        public DeferredMessagePlugin(IMessageReceiver messageReceiver)
+        {
+            _messageReceiver = messageReceiver;
+        }
+
+        public override Task<Message> AfterMessageReceive(Message message)
+        {
+            return _messageReceiver.GetDeferredMessageAsync(message);
+        }
+    }
+}
---
+++ tests/MooseSoft.Azure.ServiceBus.Tests/DeferredMessagePluginTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Microsoft.Azure.ServiceBus.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace MooseSoft.Azure.ServiceBus.Tests
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class DeferredMessagePluginTests : MessageTestBase
+    {
+        [TestMethod]
+        public async Task AfterMessageReceive_Test()
+        {
+            //Arrange
+            var receiver = Substitute.For<IMessageReceiver>();
+            var message = CreateMessage();
+            message.UserProperties.Add(Constants.DeferredKey, long.MaxValue);
+            var deferredMessage = CreateMessage();
+            receiver.ReceiveDeferredMessageAsync(long.MaxValue).Returns(deferredMessage);
+
+            var sut = new DeferredMessagePlugin(receiver);
+
+            //Act
+            var result = await sut.AfterMessageReceive(message).ConfigureAwait(false);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.MessageId.Should().NotBeNullOrWhiteSpace()
+                .And
+                .Be(deferredMessage.MessageId);
+
+            result.SystemProperties.Should().NotBeNull();
+            result.SystemProperties.LockToken.Should().NotBeNullOrWhiteSpace()
+                .And
+                .Be(deferredMessage.SystemProperties.LockToken);
+
+            await receiver.Received().CompleteAsync(Arg.Is(message.SystemProperties.LockToken)).ConfigureAwait(false);
+            await receiver.Received().ReceiveDeferredMessageAsync(Arg.Is(long.MaxValue)).ConfigureAwait(false);
+        }
+    }
+}
