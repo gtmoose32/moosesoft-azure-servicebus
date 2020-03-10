c98ac3bb09224ac49c351c1859167d5d97c740d7
Marty Mathis <Marty.Mathis@assurant.com>
2020-03-10 07:15:28 -0400

    Added MessageReceiver extensions for deferred message handling through plugin support.

---
+++ src/MooseSoft.Azure.ServiceBus/MessageReceiverExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace MooseSoft.Azure.ServiceBus
+{
+    public static class MessageReceiverExtensions
+    {
+        public static IMessageReceiver AddDeferredMessagePlugin(this IMessageReceiver messageReceiver)
+        {
+            messageReceiver.RegisterPlugin(new DeferredMessagePlugin(messageReceiver));
+
+            return messageReceiver;
+        }
+
+        public static async Task<Message> GetDeferredMessageAsync(this IMessageReceiver messageReceiver, Message message)
+        {
+            var sequenceNumber = message.GetDeferredSequenceNumber();
+            if (!sequenceNumber.HasValue) return message;
+
+            using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                await messageReceiver.CompleteAsync(message.SystemProperties.LockToken).ConfigureAwait(false);
+                message = await messageReceiver.ReceiveDeferredMessageAsync(sequenceNumber.Value)
+                    .ConfigureAwait(false);
+
+                scope.Complete();
+            }
+
+            return message;
+        }
+    }
+}
---
+++ tests/MooseSoft.Azure.ServiceBus.Tests/MessageReceiverExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Azure.ServiceBus.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MooseSoft.Azure.ServiceBus.Tests
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class MessageReceiverExtensionsTests
+    {
+        [TestMethod]
+        public void AddDeferredMessagePlugin_Test()
+        {
+            //Arrange
+            var sut = Substitute.For<IMessageReceiver>();
+
+            //Act
+            sut.AddDeferredMessagePlugin();
+
+            //Assert
+            sut.Received().RegisterPlugin(Arg.Any<DeferredMessagePlugin>());
+        }
+
+        //public static IMessageReceiver AddDeferredMessagePlugin(this IMessageReceiver messageReceiver)
+        //{
+        //    messageReceiver.RegisterPlugin(new DeferredMessagePlugin(messageReceiver));
+
+        //    return messageReceiver;
+        //}
+
+        //public static async Task<Message> GetDeferredMessageAsync(this IMessageReceiver messageReceiver, Message message)
+        //{
+        //    var sequenceNumber = message.GetDeferredSequenceNumber();
+        //    if (!sequenceNumber.HasValue) return message;
+
+        //    using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+        //    {
+        //        await messageReceiver.CompleteAsync(message.SystemProperties.LockToken).ConfigureAwait(false);
+        //        message = await messageReceiver.ReceiveDeferredMessageAsync(sequenceNumber.Value)
+        //            .ConfigureAwait(false);
+
+        //        scope.Complete();
+        //    }
+
+        //    return message;
+        //}
+    }
+}
