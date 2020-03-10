99d31b6159d76f49be42904e493f9073d9886b48
Marty Mathis <Marty.Mathis@assurant.com>
2020-03-10 18:53:17 -0400

    Added new message pump sample.

---
+++ samples/MessagePumpConsoleSample/SampleMessageProcessor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.ServiceBus;
+using MooseSoft.Azure.ServiceBus.Abstractions;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MessagePumpConsoleSample
+{
+    /// <summary>
+    /// This class should contain code custom code necessary to "process" the message received from Service Bus
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class SampleMessageProcessor : IMessageProcessor
+    {
+        public Task ProcessMessageAsync(Message message, CancellationToken cancellationToken)
+        {
+            throw new InvalidOperationException("Test failure policy with back off delay strategy.");
+        }
+    }
+}
