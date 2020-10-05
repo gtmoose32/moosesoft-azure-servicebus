6e771a209f492016fbd9a1e3b90b393ca1b82b9b
Marty Mathis <Marty.Mathis@assurant.com>
2020-10-05 08:40:42 -0400

    Updated Azure Function sample

---
+++ samples/AzureFunctionSample/Startup.cs
@@ -0,0 +1,24 @@
+﻿using AzureFunctionSample;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Moosesoft.Azure.ServiceBus;
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace AzureFunctionSample
+{
+    [ExcludeFromCodeCoverage]
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton(
+                Builder.MessageContextProcessor
+                    .Configure()
+                    .WithMessageProcessor<SampleMessageProcessor>()
+                    .WithCloneMessageFailurePolicy()
+                    .WithExponentialBackOffDelayStrategy()
+                    .Build());
+        }
+    }
+}
